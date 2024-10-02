9a56d9219af7e99806b205659be458c310ac6b33
Merab16 <merabsardlishvili@gmail.com>
2024-10-02 19:19:36 +0300

    Added glfw and spdlog to project

---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+
+#include <SimpleEngineCore/Application.hpp>
+
+class MyApp : public SimpleEngine::Application
+{
+    virtual void OnUpdate() override
+    {
+    //    std::cout << "Update frame: " << frame++ << std::endl;
+    }
+
+    int frame = 0;
+};
+
+
+// переделать под свой стиль и залить на гитхаб
+
+int main()
+{
+    auto myApp = std::make_unique<MyApp>();
+
+    int returnCode = myApp->Start(800, 600, "My first App");
+
+    std::cin.get();
+
+    return returnCode;
+}
